adae655a459b6a709fdaa89c14b9055e17f89300
Eric Pham <eric02pham@gmail.com>
2026-09-17 17:45:53 -0700

    Update vulnerable_test_pycfg.py

--- vulnerable_test_pycfg.py
+++ vulnerable_test_pycfg.py
@@ -1,5 +1,20 @@
 import os
 #random git comments 
+# -----------------------------
+# 1. INDIRECT TAINT THROUGH VARIABLES
+# -----------------------------
+
+def indirect_flow():
+    user_input = input("cmd: ")fjwenjnk
+
+    # AST sees only variable assignments + os.system("x")
+    # but DOES NOT know "cmd" is tainted
+    cmd = user_input
+    safe_cmd = cmd
+    final_cmd = safe_cmd
+
+    os.system(final_cmd)  # PyCFG must trace taint across multiple hops
+
 
 # -----------------------------
 # 2. TAINT THROUGH CONDITIONAL LOGIC
